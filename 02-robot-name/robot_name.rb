a910146dc4e3d16dd88ad6a41c217539d7a54a5c
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-11 10:08:01 -0700

    Unskipped specs with reset

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -5,10 +5,10 @@ class Robot
 
   attr_reader :name
   def initialize
-    @name = get_new_name
+    @name = reset
   end
 
-  def get_new_name
+  def reset
     @name = Faker::Base.regexify(NAME_REGEXP)
   end
 
