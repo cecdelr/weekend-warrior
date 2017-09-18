6a7060dfa8cffe0b075cdd4701c99e58051d5ea4
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-18 01:31:19 -0700

    word count weekend warrior done

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
-
+def words(phrase)
+  words = phrase.split
+  words_hash = Hash.new
+  words.each do |word|
+    if words_hash.keys.include?(word)
+      words_hash[word] += 1
+    else
+      words_hash.merge!({word => 1 })
+    end
+  end
+  return words_hash
+end
